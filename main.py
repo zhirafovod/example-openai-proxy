5a85e7aadbd45f57c9a2b878b37203acd911a399
Sergey Sergeev <zhirafovod@gmail.com>
2023-07-08 10:55:45 -0700

    working (starting) version of regular completion api

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from api.endpoints import chat_completion, completion
+from api.endpoints import chat_completions, completions
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 app = FastAPI()
 
 FastAPIInstrumentor.instrument_app(app)
 
-# app.include_router(completion.router, prefix="/openai/deployments/{deployment_id}/completions")", tags=["completion"])
-app.include_router(chat_completion.router, prefix="/openai/deployments/{deployment_id}/chat/completions", tags=["chat_completion"])
+app.include_router(completions.router, prefix="/openai/deployments/{deployment_id}/completions)", tags=["completion"])
+app.include_router(chat_completions.router, prefix="/openai/deployments/{deployment_id}/chat/completions", tags=["chat_completion"])
